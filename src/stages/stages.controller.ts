cf2816d8d3925996a09789f025fc1553ef0f1cb9
rtimur25 <rylovtimur@mail.ru>
2022-09-06 14:32:23 +0300

    stages controller and service

--- src/stages/stages.controller.ts
+++ src/stages/stages.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwtAuth.guard';
+import { StagesService } from './stages.service';
 
+@ApiTags('Stages')
+@ApiSecurity('JWT token', ['JWT token'])
+@UseGuards(JwtAuthGuard)
 @Controller('stages')
-export class StagesController {}
+@ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
+export class StagesController {
+  constructor(private readonly stagesService: StagesService) {}
+
+  @Get(':eventId')
+  @ApiParam({
+    name: 'eventId',
+    required: true,
+    description: 'Идентификатор мероприятия',
+  })
+  @ApiOperation({ summary: 'Получить все этапы по мероприятию' })
+  async findAll(@Param() params: { eventId: number }) {
+    return await this.stagesService.findAllByEventId(params.eventId);
+  }
+}
--- src/stages/stages.service.ts
+++ src/stages/stages.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Stages } from './stages.model';
 
 @Injectable()
-export class StagesService {}
+export class StagesService {
+  constructor(
+    @InjectModel(Stages)
+    private stagesModel: typeof Stages,
+  ) {}
+
+  /** Получить все этапы по мероприятию */
+  async findAllByEventId(eventId: number) {
+    const stages = await this.stagesModel.findAll({
+      where: {
+        eventId: eventId,
+      },
+    });
+    return stages;
+  }
+}
